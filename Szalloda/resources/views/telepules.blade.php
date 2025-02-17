675500e08b40dca8d0e8a7e23086ce0a4fc4ee00
Depymasut32 <babence2005@gmail.com>
2025-02-17 13:00:11 +0100

    Telepules blade

---
+++ Szalloda/resources/views/telepules.blade.php
@@ -0,0 +1,135 @@
+@extends('layout')
+@section('content')
+<div class="mainContent">
+    <section>
+        <h1>település neve</h1>
+        <img src="https://placehold.co/600x400" alt="szalloda_kep_0" title="Szálloda"
+            class="szallodaMainImg img-fluid">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nemo consequuntur perferendis ducimus
+            quam
+            autem
+            suscipit atque sit laudantium, ea harum delectus, cum cupiditate quos nihil. Atque cupiditate odit
+            ullam.
+        </p>
+    </section>
+    <div class="citySection">
+        <div class="city">
+            <h2>Szálloda 1</h2>
+            <div class="cityImgContainer">
+                <img src="https://placehold.co/400" alt="telepules" title="Település" class="img-fluid">
+                <p><span class="starTicked">★★★★★</span></p>
+            </div>
+            <p style="float: none;">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula varius magna. Praesent
+                consequat, eros vel iaculis efficitur, ante odio rutrum ex, eget finibus elit justo nec orci.
+                Maecenas ultricies auctor lorem quis elementum. Aenean porttitor erat enim, vitae porta nunc
+                pretium vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium diam
+                quis felis mollis luctus. Nam rutrum tincidunt placerat. Orci varius natoque penatibus et magnis
+                dis parturient montes, nascetur ridiculus mus.
+
+                Cras in ex id tortor scelerisque finibus in et leo. Proin sit amet ligula fermentum, dapibus
+                massa non, elementum felis. Curabitur sed efficitur felis. Integer laoreet et ex in placerat.
+                Pellentesque ultrices bibendum efficitur. Aenean dolor erat, porta quis urna cursus, efficitur
+                rutrum libero. Nullam imperdiet nulla in egestas gravida. Sed sagittis vitae lectus sed maximus.
+
+                Aliquam erat volutpat. Nulla finibus, tortor id laoreet viverra, elit ante sollicitudin turpis,
+                in imperdiet arcu tellus sit amet risus. Orci varius natoque penatibus et magnis dis parturient
+                montes, nascetur ridiculus mus. Cras nisl ipsum, dapibus nec mauris non, egestas congue justo.
+                Mauris dolor massa, condimentum eget sollicitudin ut, tincidunt vel nulla. Ut sit amet auctor
+                nulla. Nullam posuere purus quam, ac dictum nunc semper at. Etiam et rhoncus nunc. Morbi sed
+                purus in augue porta ultricies. Quisque ut viverra odio. Integer felis turpis, placerat in
+                convallis eget, luctus placerat metus. Interdum et malesuada fames ac ante ipsum primis in
+                faucibus. Vestibulum orci tortor, blandit nec pharetra a, bibendum id mi.
+            </p>
+        </div>
+        <div class="city">
+            <h2>Szálloda 2</h2>
+            <div class="cityImgContainer">
+                <img src="https://placehold.co/400" alt="telepules" title="Település" class="img-fluid">
+                <p><span class="starTicked">★★★</span><span class="starUnTicked">★★</span></p>
+            </div>
+            <p style="float: none;">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula varius magna. Praesent
+                consequat, eros vel iaculis efficitur, ante odio rutrum ex, eget finibus elit justo nec orci.
+                Maecenas ultricies auctor lorem quis elementum. Aenean porttitor erat enim, vitae porta nunc
+                pretium vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium diam
+                quis felis mollis luctus. Nam rutrum tincidunt placerat. Orci varius natoque penatibus et magnis
+                dis parturient montes, nascetur ridiculus mus.
+
+                Cras in ex id tortor scelerisque finibus in et leo. Proin sit amet ligula fermentum, dapibus
+                massa non, elementum felis. Curabitur sed efficitur felis. Integer laoreet et ex in placerat.
+                Pellentesque ultrices bibendum efficitur. Aenean dolor erat, porta quis urna cursus, efficitur
+                rutrum libero. Nullam imperdiet nulla in egestas gravida. Sed sagittis vitae lectus sed maximus.
+
+                Aliquam erat volutpat. Nulla finibus, tortor id laoreet viverra, elit ante sollicitudin turpis,
+                in imperdiet arcu tellus sit amet risus. Orci varius natoque penatibus et magnis dis parturient
+                montes, nascetur ridiculus mus. Cras nisl ipsum, dapibus nec mauris non, egestas congue justo.
+                Mauris dolor massa, condimentum eget sollicitudin ut, tincidunt vel nulla. Ut sit amet auctor
+                nulla. Nullam posuere purus quam, ac dictum nunc semper at. Etiam et rhoncus nunc. Morbi sed
+                purus in augue porta ultricies. Quisque ut viverra odio. Integer felis turpis, placerat in
+                convallis eget, luctus placerat metus. Interdum et malesuada fames ac ante ipsum primis in
+                faucibus. Vestibulum orci tortor, blandit nec pharetra a, bibendum id mi.
+            </p>
+        </div>
+        <div class="city">
+            <h2>Szálloda 3</h2>
+            <div class="cityImgContainer">
+                <img src="https://placehold.co/400" alt="telepules" title="Település" class="img-fluid">
+                <p><span class="starTicked">★★★★</span><span class="starUnTicked">★</span></p>
+            </div>
+            <p style="float: none;">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula varius magna. Praesent
+                consequat, eros vel iaculis efficitur, ante odio rutrum ex, eget finibus elit justo nec orci.
+                Maecenas ultricies auctor lorem quis elementum. Aenean porttitor erat enim, vitae porta nunc
+                pretium vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium diam
+                quis felis mollis luctus. Nam rutrum tincidunt placerat. Orci varius natoque penatibus et magnis
+                dis parturient montes, nascetur ridiculus mus.
+
+                Cras in ex id tortor scelerisque finibus in et leo. Proin sit amet ligula fermentum, dapibus
+                massa non, elementum felis. Curabitur sed efficitur felis. Integer laoreet et ex in placerat.
+                Pellentesque ultrices bibendum efficitur. Aenean dolor erat, porta quis urna cursus, efficitur
+                rutrum libero. Nullam imperdiet nulla in egestas gravida. Sed sagittis vitae lectus sed maximus.
+
+                Aliquam erat volutpat. Nulla finibus, tortor id laoreet viverra, elit ante sollicitudin turpis,
+                in imperdiet arcu tellus sit amet risus. Orci varius natoque penatibus et magnis dis parturient
+                montes, nascetur ridiculus mus. Cras nisl ipsum, dapibus nec mauris non, egestas congue justo.
+                Mauris dolor massa, condimentum eget sollicitudin ut, tincidunt vel nulla. Ut sit amet auctor
+                nulla. Nullam posuere purus quam, ac dictum nunc semper at. Etiam et rhoncus nunc. Morbi sed
+                purus in augue porta ultricies. Quisque ut viverra odio. Integer felis turpis, placerat in
+                convallis eget, luctus placerat metus. Interdum et malesuada fames ac ante ipsum primis in
+                faucibus. Vestibulum orci tortor, blandit nec pharetra a, bibendum id mi.
+            </p>
+        </div>
+        <div class="city">
+            <h2>Szálloda 4</h2>
+            <div class="cityImgContainer">
+                <img src="https://placehold.co/400" alt="telepules" title="Település" class="img-fluid">
+                <p><span class="starTicked">★</span><span class="starUnTicked">★★★★</span></p>
+            </div>
+            <p style="float: none;">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula varius magna. Praesent
+                consequat, eros vel iaculis efficitur, ante odio rutrum ex, eget finibus elit justo nec orci.
+                Maecenas ultricies auctor lorem quis elementum. Aenean porttitor erat enim, vitae porta nunc
+                pretium vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium diam
+                quis felis mollis luctus. Nam rutrum tincidunt placerat. Orci varius natoque penatibus et magnis
+                dis parturient montes, nascetur ridiculus mus.
+
+                Cras in ex id tortor scelerisque finibus in et leo. Proin sit amet ligula fermentum, dapibus
+                massa non, elementum felis. Curabitur sed efficitur felis. Integer laoreet et ex in placerat.
+                Pellentesque ultrices bibendum efficitur. Aenean dolor erat, porta quis urna cursus, efficitur
+                rutrum libero. Nullam imperdiet nulla in egestas gravida. Sed sagittis vitae lectus sed maximus.
+
+                Aliquam erat volutpat. Nulla finibus, tortor id laoreet viverra, elit ante sollicitudin turpis,
+                in imperdiet arcu tellus sit amet risus. Orci varius natoque penatibus et magnis dis parturient
+                montes, nascetur ridiculus mus. Cras nisl ipsum, dapibus nec mauris non, egestas congue justo.
+                Mauris dolor massa, condimentum eget sollicitudin ut, tincidunt vel nulla. Ut sit amet auctor
+                nulla. Nullam posuere purus quam, ac dictum nunc semper at. Etiam et rhoncus nunc. Morbi sed
+                purus in augue porta ultricies. Quisque ut viverra odio. Integer felis turpis, placerat in
+                convallis eget, luctus placerat metus. Interdum et malesuada fames ac ante ipsum primis in
+                faucibus. Vestibulum orci tortor, blandit nec pharetra a, bibendum id mi.
+            </p>
+        </div>
+    </div>
+</div>
+@endsection
