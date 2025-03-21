f232cc68f7bc827d5f76a335391595dac384c5b5
Vajdani <50171679+Vajdani@users.noreply.github.com>
2025-03-21 11:30:18 +0100

    wip form refactor

---
+++ SzallodaManagerForm/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace SzallodaManagerForm.Models
+{
+    internal class Employee
+    {
+        public string username { get; private set; }
+        public int hotel_id { get; private set; }
+        public User.AuthorityLevel AuthorityLevel { get; private set; }
+
+        public Employee(Database DB)
+        {
+            username = DB.GetString("username");
+            hotel_id = DB.GetInt("hotel_id");
+            AuthorityLevel = User.GetAuthorityLevel(DB.GetString("userType"));
+        }
+    }
+}
