f894e4e03603a803e5a12baedabde51a365861ff
Vajdani <50171679+Vajdani@users.noreply.github.com>
2025-03-31 10:16:05 +0200

    fix profile menus not closing

--- Szalloda/public/js/profile.js
+++ Szalloda/public/js/profile.js
@@ -1,49 +1,49 @@
-function point(point){
+function point(point) {
     slider = document.getElementById("point")
-    slider.value=point
+    slider.value = point
 }
 
-function loyaltymenu(loyalty,points,currentmin,nextmin){
+function loyaltymenu(loyalty, points, currentmin, nextmin) {
     let panel = document.createElement("div")
-    panel.id="loyaltymenu"
+    panel.id = "loyaltymenu"
     panel.className = "menuBgOverlay"
-    panel.onclick = CloseMenu
-    
+    // panel.onclick = () => CloseMenu('loyaltymenu')
+
     panel.innerHTML = `
     <div class="ProfilePicture-Selection" id="loyal">
         <div class="PPS-head">
-            <h2>Az ön jelenlegi rangja: `+loyalty+`</h2>
+            <h2>Az ön jelenlegi rangja: `+ loyalty + `</h2>
         </div>
         <hr> 
         <div class="loyal-body">
-           <p>`+currentmin+`</p><progress id="point" class="pointslider" value="`+points+`" min="`+currentmin+`" max="`+nextmin+`"></progress>  <p style="float: right">`+nextmin+`</p>
+           <p>`+ currentmin + `</p><progress id="point" class="pointslider" value="` + points + `" min="` + currentmin + `" max="` + nextmin + `"></progress>  <p style="float: right">` + nextmin + `</p>
         </div>  
         <div class="PPS-buttons">
-            <button class="save-button" onclick="CloseMenu('loyaltymenu')">Mégse</button>
+            <button class="save-button" onclick="CloseMenu('loyaltymenu')">Bezárás</button>
         </div>
     </div>
     `
     document.body.appendChild(panel)
-   
+
 }
 
-function loyaltymax(loyalty){
+function loyaltymax(loyalty) {
     let panel = document.createElement("div")
-    panel.id="loyaltymenu"
+    panel.id = "loyaltymenu"
     panel.className = "menuBgOverlay"
-    panel.onclick = CloseMenu
-    
+    // panel.onclick = () => CloseMenu('loyaltymenu')
+
     panel.innerHTML = `
     <div class="ProfilePicture-Selection" id="loyal">
         <div class="PPS-head">
-            <h2>Az ön jelenlegi rangja: `+loyalty+`</h2>
+            <h2>Az ön jelenlegi rangja: `+ loyalty + `</h2>
         </div>
         <hr> 
         <div class="loyal-body">
             <p>Gratulálok, elérted a legmagasabb rangot!</p>
         </div>  
         <div class="PPS-buttons">
-            <button class="save-button" onclick="CloseMenu('loyaltymenu')">Mégse</button>
+            <button class="save-button" onclick="CloseMenu('loyaltymenu')">Bezárás</button>
         </div>
     </div>
     `
@@ -54,7 +54,7 @@ function pfpmenu() {
     let panel = document.createElement("div")
     panel.id = "pfpMenu"
     panel.className = "menuBgOverlay"
-    //panel.onclick = CloseMenu
+    //panel.onclick = () => CloseMenu('pfpMenu')
 
     let pfps = `<div class="PPS-body">`
     for (let i = 0; i < 8; i++) {
@@ -101,6 +101,7 @@ function CloseMenu(menuid) {
 
 document.addEventListener("keydown", (e) => {
     if (e.key == "Escape") {
-        CloseMenu()
+        CloseMenu("pfpMenu")
+        CloseMenu("loyaltymenu")
     }
 })
