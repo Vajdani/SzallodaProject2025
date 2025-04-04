478c8ba727e1ce8cfc95792fbc65010f4e0cd81a
Kovács Mihály <117653038+Misike28@users.noreply.github.com>
2025-04-04 11:29:24 +0200

    minor documentation, env example, stat form

--- SzallodaManagerForm/hotelstat.Designer.cs
+++ SzallodaManagerForm/hotelstat.Designer.cs
@@ -33,6 +33,10 @@
             Month = new DataGridViewTextBoxColumn();
             count = new DataGridViewTextBoxColumn();
             income = new DataGridViewTextBoxColumn();
+            lbevsum = new Label();
+            cbevek = new ComboBox();
+            lbpopular = new Label();
+            lbmax = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvadatok).BeginInit();
             SuspendLayout();
             // 
@@ -40,9 +44,9 @@
             // 
             lbHotel.AutoSize = true;
             lbHotel.Font = new Font("Segoe UI", 20F);
-            lbHotel.Location = new Point(40, 9);
+            lbHotel.Location = new Point(35, 52);
             lbHotel.Name = "lbHotel";
-            lbHotel.Size = new Size(97, 46);
+            lbHotel.Size = new Size(78, 37);
             lbHotel.TabIndex = 2;
             lbHotel.Text = "hotel";
             // 
@@ -50,10 +54,11 @@
             // 
             dgvadatok.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvadatok.Columns.AddRange(new DataGridViewColumn[] { Month, count, income });
-            dgvadatok.Location = new Point(40, 58);
+            dgvadatok.Location = new Point(35, 89);
+            dgvadatok.Margin = new Padding(3, 2, 3, 2);
             dgvadatok.Name = "dgvadatok";
             dgvadatok.RowHeadersWidth = 51;
-            dgvadatok.Size = new Size(475, 454);
+            dgvadatok.Size = new Size(475, 340);
             dgvadatok.TabIndex = 4;
             // 
             // Month
@@ -80,13 +85,54 @@
             income.ReadOnly = true;
             income.Width = 125;
             // 
+            // lbevsum
+            // 
+            lbevsum.AutoSize = true;
+            lbevsum.Location = new Point(534, 102);
+            lbevsum.Name = "lbevsum";
+            lbevsum.Size = new Size(141, 15);
+            lbevsum.TabIndex = 5;
+            lbevsum.Text = "Év eddigi összes bevétele:";
+            // 
+            // cbevek
+            // 
+            cbevek.FormattingEnabled = true;
+            cbevek.Location = new Point(35, 26);
+            cbevek.Name = "cbevek";
+            cbevek.Size = new Size(121, 23);
+            cbevek.TabIndex = 6;
+            cbevek.SelectedIndexChanged += cbevek_SelectedIndexChanged;
+            // 
+            // lbpopular
+            // 
+            lbpopular.AutoSize = true;
+            lbpopular.Location = new Point(534, 162);
+            lbpopular.Name = "lbpopular";
+            lbpopular.Size = new Size(137, 15);
+            lbpopular.TabIndex = 7;
+            lbpopular.Text = "legnépszerűbb hónapok:";
+            // 
+            // lbmax
+            // 
+            lbmax.AutoSize = true;
+            lbmax.Location = new Point(534, 220);
+            lbmax.Name = "lbmax";
+            lbmax.Size = new Size(147, 15);
+            lbmax.TabIndex = 8;
+            lbmax.Text = "Legjövedelmezőbb hónap:";
+            // 
             // hotelstat
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(549, 550);
+            ClientSize = new Size(838, 510);
+            Controls.Add(lbmax);
+            Controls.Add(lbpopular);
+            Controls.Add(cbevek);
+            Controls.Add(lbevsum);
             Controls.Add(dgvadatok);
             Controls.Add(lbHotel);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "hotelstat";
             StartPosition = FormStartPosition.CenterParent;
             Text = "Statisztika";
@@ -102,5 +148,9 @@
         private DataGridViewTextBoxColumn Month;
         private DataGridViewTextBoxColumn count;
         private DataGridViewTextBoxColumn income;
+        private Label lbevsum;
+        private ComboBox cbevek;
+        private Label lbpopular;
+        private Label lbmax;
     }
 }
--- SzallodaManagerForm/hotelstat.cs
+++ SzallodaManagerForm/hotelstat.cs
@@ -21,24 +21,47 @@ namespace SzallodaManagerForm
             InitializeComponent();
         }
 
-        public void load()
+        public void year()
+        {
+            cbevek.Items.Clear();
+            string leker = $"select distinct year(b.bookStart) as ev from booking b " +
+                    $"inner join room r on b.room_id = r.room_id " +
+                    $"inner join hotel h on r.hotel_id = h.hotel_id " + 
+                    $" where h.hotelName like \"{hotelname}\" order by ev desc";
+            Database ab = new Database(leker);
+            while (ab.Read())
+            {
+                cbevek.Items.Add(ab.Reader["ev"].ToString());
+            }
+            cbevek.SelectedIndex = 0;
+        }
+
+        public void load(int ev)
         {
             dgvadatok.Rows.Clear();
             string[] honapok = ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"];
-       
-            
-            for(int i = 1; i < 13; i++) { 
-            
+
+            //int jelenleg = DateTime.Now.Month;
+            int evesosszeg = 0;
+            int maxdb = 0;
+            int maxft = 0;
+            string maxfthonap = "";
+            List<string> maxhonap = new List<string>();
+            for (int i = 1; i < 13; i++)
+            {
+                string honap = i.ToString("D2");
+
                 string leker = $"select sum(b.totalPrice) as sum,Count(b.booking_id) as db " +
                     $"from booking b " +
                     $"inner join room r on b.room_id = r.room_id " +
                     $"inner join hotel h on r.hotel_id = h.hotel_id " +
-                    $"where b.bookStart like '____-%{i}-__' " +
+                    $"where b.bookStart like '{ev}-{honap}-__' " +
                     $"and h.hotelName like \"{hotelname}\"";
+         
                 Database ab = new Database(leker);
                 ab.Read();
                 string sum = "";
-                if (ab.Reader["db"].ToString() =="0")
+                if (ab.Reader["db"].ToString() == "0")
                 {
                     sum = "0";
                 }
@@ -46,14 +69,68 @@ namespace SzallodaManagerForm
                 {
                     sum = ab.Reader["sum"].ToString();
                 }
-                dgvadatok.Rows.Add(honapok[i-1], ab.Reader["db"], sum+" Ft");
+
+                if (Convert.ToInt32(ab.Reader["db"]) == maxdb)
+                {
+                    maxhonap.Add(honapok[i-1]);
+                    maxdb = Convert.ToInt32(ab.Reader["db"]);
+                }
+                else if (Convert.ToInt32(ab.Reader["db"]) > maxdb)
+                {
+                    maxhonap.Clear();
+                    maxhonap.Add(honapok[i - 1]);
+                    maxdb = Convert.ToInt32(ab.Reader["db"]);
+                }
+                if (ab.Reader["sum"].ToString() != null)
+                {
+                    if (Convert.ToInt32(ab.Reader["sum"]) > maxft)
+                    {
+                        maxft = Convert.ToInt32(ab.Reader["sum"]);
+                        maxfthonap = honapok[i - 1];
+                    }
+                }
+                evesosszeg += Convert.ToInt32(sum);
+                dgvadatok.Rows.Add(honapok[i - 1], ab.Reader["db"], sum + " Ft");
+                ab.Close();
             }
+  
+            if (ev >= DateTime.Now.Year)
+            {
+                lbevsum.Text = "Az év eddigi teljes bevétele: " + evesosszeg+" forint";
+            }
+            else if (ev < DateTime.Now.Year)
+            {
+                lbevsum.Text = "Az év teljes bevétele: " + evesosszeg +" forint";
+            }
+            if (maxhonap.Count() == 1)
+            {
+                lbpopular.Text = $"Legnépszerűbb hónap: {maxhonap[0]}, {maxdb} foglalással";
+            }
+            else {
+                lbpopular.Text = $"Legnépszerűbb hónapok: ";
+                foreach (var m in maxhonap)
+                {
+                    lbpopular.Text = lbpopular.Text + m+", ";   
+                }
+                lbpopular.Text = lbpopular.Text + $" {maxdb} foglalással";
+            }
+
+            lbmax.Text = $"A legjövedelmezőbb hónap: {maxfthonap}, {maxft} forint jövedelemmel";
+  
         }
 
         private void hotelstat_Load(object sender, EventArgs e)
         {
+           
             lbHotel.Text = hotelname;
-            load();
+            load(DateTime.Now.Year);
+            year();
+        }
+
+        private void cbevek_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int ev = Convert.ToInt32(cbevek.SelectedItem);
+            load(ev);
         }
     }
 }
