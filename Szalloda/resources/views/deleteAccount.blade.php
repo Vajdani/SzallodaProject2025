31a3af112bc52cfef1740a9ecc47d4b225cf8506
Vajdani <50171679+Vajdani@users.noreply.github.com>
2025-04-14 08:53:25 +0200

    account delete page styling

--- Szalloda/resources/views/deleteAccount.blade.php
+++ Szalloda/resources/views/deleteAccount.blade.php
@@ -2,6 +2,7 @@
 
 @section('css')
     <link rel="stylesheet" href="{{ asset('css/bejelentkezes.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/fioktorles.css') }}">
 @endsection
 
 @section('content')
@@ -10,19 +11,21 @@
     </nav>
     <div class="mainContent">
         <section>
-            <div class="center form">
+            <div class="center form" style="height:unset">
                 <h2 class="text-center">Biztos hogy meg szeretne válni a fiókjától?</h2>
                 <hr>
-                <form action="/fioktorles/megerositem" method="POST" class="delete-form center">
-                    @csrf
-                    <div class="text-center">
-                        <input type="checkbox" name="deleteReviews" id="deleteReviews">
-                        <label for="deleteReviews">Törlöm az értékeléseimet is</label>
+                <div class="formBox">
+                    <form action="/fioktorles/megerositem" method="POST">
+                        @csrf
+                        <div class="text-center">
+                            <input type="checkbox" name="deleteReviews" id="deleteReviews">
+                            <label for="deleteReviews">Törlöm az értékeléseimet is</label>
+                        </div>
+                        <input type="submit" class="delete-button" value="Igen, törlöm a fiókom">
+                    </form>
+                    <div>
+                        <a href="/profil"><button class="save-button">Nem, mégsem törlöm a fiókom</button></a>
                     </div>
-                    <input type="submit" class="delete-button" value="Igen, törlöm a fiókom">
-                </form>
-                <div class="text-center">
-                    <a href="/profil"><button class="save-button">Nem, mégsem törlöm a fiókom</button></a>
                 </div>
             </div>
         </section>
